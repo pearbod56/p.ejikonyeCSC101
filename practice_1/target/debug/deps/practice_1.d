5d2145811abf70e2e5522b03ab80270b55e19fab
Pearl Ejikonye <115809223+pearbod56@users.noreply.github.com>
2022-11-09 14:52:43 +0100

    delete1

--- practice_1/target/debug/deps/practice_1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Pearl\Desktop\CSC101\p.ejikonyeCSC101\week-3\practice_1\target\debug\deps\practice_1.exe: src\main.rs
-
-C:\Users\Pearl\Desktop\CSC101\p.ejikonyeCSC101\week-3\practice_1\target\debug\deps\practice_1.d: src\main.rs
-
-src\main.rs:
--- practice_1/target/debug/practice_1.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Pearl\Desktop\week-3\practice_1\target\debug\practice_1.exe: C:\Users\Pearl\Desktop\week-3\practice_1\src\main.rs
