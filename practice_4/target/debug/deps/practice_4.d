d814019c85f9c7777093d2e27a144aace4b4ccf3
Pearl Ejikonye <115809223+pearbod56@users.noreply.github.com>
2022-11-09 15:00:59 +0100

    delete4

--- practice_4/target/debug/deps/practice_4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Pearl\Desktop\week-3\practice_4\target\debug\deps\practice_4.exe: src\main.rs
-
-C:\Users\Pearl\Desktop\week-3\practice_4\target\debug\deps\practice_4.d: src\main.rs
-
-src\main.rs:
